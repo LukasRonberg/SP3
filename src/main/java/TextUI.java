463534604b705194aeabf5439e469fdbdcdbce1f
LukasRonberg <143714608+LukasRonberg@users.noreply.github.com>
2023-11-16 13:08:09 +0100

    Tilføjede kode til FileIO og TextUI

--- src/main/java/TextUI.java
+++ src/main/java/TextUI.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class TextUI {
+    private Scanner scan = new Scanner(System.in);
+
+    /**
+     * shows a message and returns the user's input as a String
+     * @param msg
+     * @return
+     */
+    public String getInput(String msg) {
+        this.displayMessage(msg);
+        return scan.nextLine();
+    }
+
+    /**
+     * displays a message
+     * @param msg
+     */
+    public void displayMessage(String msg) {
+
+        System.out.println(msg);
+
+    }
 }
