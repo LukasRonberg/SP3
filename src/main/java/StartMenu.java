ad86167e76caa649272220ec3432c1dc81234933
LukasRonberg <143714608+LukasRonberg@users.noreply.github.com>
2023-12-01 13:35:39 +0100

    Lavede det nemt at skifte mellem FileIO og DBConnector

--- src/main/java/StartMenu.java
+++ src/main/java/StartMenu.java
@@ -2,28 +2,29 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class StartMenu {
-
-    private final DBConnector fileHandler = new DBConnector();
+    private final IO io = new FileIO();
+    //private final IO io = new DBConnector();
     private final TextUI textUI = new TextUI();
     private HashSet<User> userList = new HashSet<User>();
 
     public void start() {
-        userList = fileHandler.readUserData("src/main/java/userdata.txt");
+        userList = io.readUserData("src/main/java/userdata.txt");
     }
 
-    // TODO: 21-11-2023 Gør således at bruger har minimum antal tegn 
+    // TODO: 21-11-2023 Gør således at bruger har minimum antal tegn
     public void createUser() {
         String username = textUI.getInput("Please enter your username below:");
         String password = textUI.getInput("Please enter your password below:");
         start();
         boolean exists = false;
         if(userList  == null) {
-            fileHandler.saveUserData(null,new User(username, password, new ArrayList<Media>(), new ArrayList<Media>()));
+            io.saveUserData(null,new User(username, password, new ArrayList<Media>(), new ArrayList<Media>()));
             textUI.displayMessage("Congratulations your user has been created!");
         }
         else if(userList.isEmpty()){
             userList.add(new User(username, password, new ArrayList<Media>(), new ArrayList<Media>()));
-            fileHandler.saveUserData(null,new User(username, password, new ArrayList<Media>(), new ArrayList<Media>()));
+            // TODO: 01/12/2023 Fjern null og tilføj userList, hvis man bruger fileIO. Tilføj null, hvis DB.
+            io.saveUserData(userList, new User(username, password, new ArrayList<Media>(), new ArrayList<Media>()));
             textUI.displayMessage("Congratulations your user has been created!");
             return;
         } else {
@@ -37,22 +38,37 @@ public class StartMenu {
                 }
 
             }
+
+
             userList.add(new User(username, password, new ArrayList<Media>(), new ArrayList<Media>()));
-            fileHandler.saveUserData(null,new User(username, password, new ArrayList<Media>(), new ArrayList<Media>()));
-            textUI.displayMessage("Congratulations your user has been created!");
+
+
+            if(io.saveUserData(null,new User(username, password, new ArrayList<Media>(), new ArrayList<Media>()))){
+                textUI.displayMessage("Congratulations your user has been created!");
+            }
+            else{
+                textUI.displayMessage("Username already taken!");
+            }
+
 
         }
 
     }
 
     public User login() {
+
         String username = textUI.getInput("Please enter your username below:");
         String password = textUI.getInput("Please enter your password below:");
 
+
+
         for (User user : userList) {
+
             if (username.equals(user.Username) && password.equals(user.Password)) {
                 return user;
             }
+
+
         }
 
         textUI.displayMessage("The information you've entered isn't correct");
